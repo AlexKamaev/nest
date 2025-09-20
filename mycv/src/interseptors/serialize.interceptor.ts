f86d72015430301ee78b3df6d79d2acbba3804d6
AlexKamaev <alexey.kamaev@devexpress.com>
2025-09-20 13:57:22 +0400

    test

---
+++ mycv/src/interseptors/serialize.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+    UseInterceptors,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+} from "@nestjs/common";
+
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { plainToClass } from "class-transformer";
+
+export function Serialize(dto: any) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {
+    }
+
+    intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+        console.log('Before the request is handled');
+        // Run something before the request is handled by the next handler
+        return handler.handle().pipe(
+            map((data) => {
+                return plainToClass(this.dto, data, {
+                    excludeExtraneousValues: true,
+                });
+            })
+        );
+    }
+}
