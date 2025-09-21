b9f076b86fa4c3bb2396ee18d9efe254a77dd15e
AlexKamaev <alexey.kamaev@devexpress.com>
2025-09-21 15:08:05 +0400

    test

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -24,7 +24,7 @@ import { User } from "./user.entity";
 
 @Controller("auth")
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
+// @UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
