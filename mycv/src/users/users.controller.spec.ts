5e700e29fcf944ddfcd5cdb4306fb01946aa792e
AlexKamaev <alexey.kamaev@devexpress.com>
2025-09-28 11:51:30 +0400

    test

--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -35,7 +35,9 @@ describe("UsersController", () => {
     };
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -77,5 +79,13 @@ describe("UsersController", () => {
     fakeUsersService.findOne = () => null;
 
     await expect(controller.findUser("1")).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -1 };
+    const user = await controller.signin({ email: 'email', password: 'password' }, session);
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
